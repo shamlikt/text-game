0863e549a649680da6f6c49b1075feaa78f98ca3
shamlik <tshamlikt@gmail.com>
2015-11-13 16:01:47 +0530

    Game without UI,working smoothly

--- game/miSsiOn_MsP.py
+++ game/miSsiOn_MsP.py
@@ -1,13 +1,13 @@
 from sys import exit
+
+
 class Object(object):
     def __init__(self,thing,description):
         self.thing = thing
         self.description = description
 
-
     def describe(self):
-        return "%s %s "%(self.name,self.description)
-
+        return "%s %s "%(self.thing,self.description)
 
 
 class Scene(object):
@@ -17,47 +17,50 @@ class Scene(object):
         if objects == None:
             objects = []
         self.objects = objects
-        
-            
+        self.out = None
+    
     def describe(self):
         print self.name
         print self.des
-        for i in self.objects:
-            print i
+        if self.objects:
+            for i in self.objects:
+                print i.describe()
+            
     
-
-
 class Engine(object):
     def __init__(self,scene):
-        self.current=scene
+        self.current=scene        
         self.carrying = []
-        self.death = Death()
         self.stage = 1
-    
+        self.death = Death()    
+
     def do(self,command):
         if self.stage == 1:
             if 'back door' in command:
                 print " WAW.....Nice Decision"
+                self.current=self.current.out
                 self.stage=2
             elif "matchbox" in command:
-                self.carrying.append("matchbox")
                 print "Now you got matchbox"
+                self.current.objects.pop()
             else:
                 self.death.message(1)
         elif self.stage == 2:
             if 'hide' in command:
                 print "shittty smell better than half day leave "
+                self.current=self.current.out
                 self.stage = 3
             else:
                 self.death.message(2)
-
         elif self.stage == 3:
-            if ' fire alarm ' in command:
+            if 'fire' in command and self.carrying:
                 print " mind blowing idea ..but watch ur dress "
+                self.current=self.current.out
                 self.stage = 4
+            elif 'fire' in command and not self.carrying:
+                self.death.message('3a')
             else:
                 self.death.message(3)
-
         elif self.stage == 4 : 
             if 'room' in command :
                 print '*' * 20
@@ -68,34 +71,72 @@ class Engine(object):
                 self.death.message(4)
 
 
-            
-
-
-
-
-
-
-            
-
-
-            
 class Death(object):
-    def message(stage):
+    def message(self,stage):
         if stage == 1 :
             print " You r A LOOOOOOOSER ,\n GO and learn how to bunk  class "
-
-
-
+            exit()
+        elif stage == 2:
+            print "Are MAD....? \n she will kick you out...."
+            print "you lost your half day sallary..."
+            exit()
+        elif stage == '3a':
+            print "How can you triger alarm without fire....."
+            print "Remember that matchbox"
+            print "Now stuck here...LOOSER"
+            exit()
+        elif stage == 3:
+            print " OMG caught Red handed Now make some excuse"
+            exit()
+        else:
+            print " Good news You lost ua job"
+            
 
 
 def start():
-    pass
 
+    box = Object("match box","some one forgot to take after smoking")
+    scene_one = "Bike parking area"
+    des_one = "full of bikes\n two way fron door or back door"
+    bike_parking = Scene(scene_one ,des_one,[box])
+    
+    scene_two = "back room"
+    des_two = "naroow varantha , toilet on the left side, \n OMG manger is coming ...you will loos your halfday sallry if she see you "
+    back_room = Scene ( scene_two , des_two)
+    
+    scene_tree = "toilet "
+    des_tree = " fire alarm  triger or open door"
+    toilet = Scene (scene_tree, des_tree)
+
+    scene_four = "alarm "
+    des_four = "go to manager room or runn to ground "
+    alarm = Scene(scene_four, des_four)
+
+    bike_parking.out = back_room
+    back_room.out = toilet
+    toilet.out = alarm
+
+    start = Engine(bike_parking)
+    return start
 
 
 
 if __name__ == '__main__':
-    pass
+    print " Welcome to mission msp "
+    print "*" * 20
+    start = start()
+
+    while True:
+        print """"""
+        start.current.describe()
+        print """"""
+        command = raw_input (" what you gonna do ?")
+        start.do(command)
+
+
+
+
+
 
 
 
