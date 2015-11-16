7addde5b33aa32c11645c665ee466d3702e1df34
shamlik <tshamlikt@gmail.com>
2015-11-16 21:47:25 +0530

    creat and test count_neighbour

--- game/game_of_life/game_of_life.py
+++ game/game_of_life/game_of_life.py
@@ -3,3 +3,48 @@
 def isalive(grid,x,y):
     return grid[x][y] == 1
 
+
+def kill(grid,x,y):
+    grid[x][y] = 0 
+    return grid 
+
+
+def live(grid,x,y):
+    grid[x][y] = 1
+    return grid
+
+
+def generate_grid():
+        grid = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,1]]
+
+        return grid
+
+
+
+def count_neighbour(grid,x,y):
+    count = 0
+    for dx in [-1,0,1]:
+        for dy in [-1,0,1]:
+            if dx == dy == 0: #same index
+                continue
+            elif dx+x < 0 or dy+y < 0:
+                continue
+            else:
+                try:
+                    
+                    if grid[(x+dx)][(y+dy)] == 1 : 
+                        count = count + 1
+                except IndexError :
+                    continue
+    return count
+
+
+# grid = [[1,0,0],
+#             [1,0,0],
+#             [0,0,1]]
+ 
+
+# print count_neighbour(grid,0,0)
--- game/game_of_life/test_game_of_life.py
+++ game/game_of_life/test_game_of_life.py
@@ -11,4 +11,75 @@ def test_isalive():
     life = game_of_life.isalive(grid,3,3)
     assert life == True
 
+
+
+
+def test_count_neighbour():
     
+    grid = [[1,0,0,0],
+            [0,1,0,0],
+            [0,1,0,0],
+            [0,0,0,1]]
+    count1 =  game_of_life.count_neighbour(grid,0,0)
+    count2 =  game_of_life.count_neighbour(grid,0,1)
+    count3 =  game_of_life.count_neighbour(grid,0,2)
+    count4 =  game_of_life.count_neighbour(grid,0,3)
+    count5 =  game_of_life.count_neighbour(grid,1,0)
+    count6 =  game_of_life.count_neighbour(grid,1,1)
+    count7 =  game_of_life.count_neighbour(grid,1,2)
+    count8 =  game_of_life.count_neighbour(grid,1,3)
+    count9 =  game_of_life.count_neighbour(grid,2,0)
+    count10 =  game_of_life.count_neighbour(grid,2,1)
+    count11 =  game_of_life.count_neighbour(grid,2,2)
+    count12 =  game_of_life.count_neighbour(grid,2,3)
+    count13 =  game_of_life.count_neighbour(grid,3,0)
+    count14 =  game_of_life.count_neighbour(grid,3,1)
+    count15 =  game_of_life.count_neighbour(grid,3,2)
+    count16 =  game_of_life.count_neighbour(grid,3,3)
+    
+    assert count1  == 1
+    assert count2  == 2
+    assert count3  == 1
+    assert count4  == 0
+    assert count5  == 3
+    assert count6  == 2
+    assert count7  == 2
+    assert count8  == 0
+    assert count9  == 2
+    assert count10 == 1
+    assert count11 == 3
+    assert count12 == 1
+    assert count13 == 1
+    assert count14 == 1
+    assert count15 == 2
+    assert count16 == 0
+
+
+    
+    
+
+
+    
+
+
+
+    
+
+
+    
+
+
+    
+
+
+    
+
+
+    
+
+
+
+
+
+    
+
